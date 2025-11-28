c8c41f553a7af97109ff8a876d2c2207940e6ee3
joknic4 <marko.joknic@gmail.com>
2025-11-28 15:48:04 +0100

    +konzolni meni -privremeno resenje
    dodat konzolni meni za sda preko njega da se testira posle ide interfejs

--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -2,6 +2,7 @@ using System;
 using HotelReservationSystem.Models;
 using HotelReservationSystem.Repositories;
 using HotelReservationSystem.Services;
+using HotelReservationSystem.UI;
 
 namespace HotelReservationSystem
 {
@@ -9,9 +10,6 @@ namespace HotelReservationSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hotel Reservation System - v4");
-            Console.WriteLine("==============================\n");
-            
             // Inicijalizacija
             var korisnikRepo = new KorisnikRepository();
             var hotelRepo = new HotelRepository();
@@ -20,32 +18,25 @@ namespace HotelReservationSystem
             var korisnikService = new KorisnikService(korisnikRepo);
             var hotelService = new HotelService(hotelRepo);
             
-            // Test registracije
-            Console.WriteLine("Registracija novog gosta...");
-            bool uspesno = korisnikService.RegistrujGosta(
-                "9876543210987", "marko@email.com", "sifra123",
-                "Marko", "Markovic", "0641234567");
-            
-            if (uspesno)
-                Console.WriteLine("Gost uspesno registrovan!\n");
-            
-            // Test logina
-            Console.WriteLine("Pokusaj prijavljivanja...");
-            if (authService.Login("marko@email.com", "sifra123"))
+            // Dodaj test podatke ako ne postoje
+            if (korisnikRepo.GetAll().Count == 0)
             {
-                var korisnik = authService.GetTrenutniKorisnik();
-                Console.WriteLine($"Uspesno prijavljen: {korisnik?.GetIme()} {korisnik?.GetPrezime()}\n");
+                var admin = new Korisnik("1234567890123", "admin@hotel.com", "admin", 
+                                         "Admin", "Adminovic", "0641111111", KorisnikTip.Administrator);
+                korisnikRepo.Add(admin);
+                korisnikRepo.Save();
             }
             
-            // Test dodavanja hotela
-            hotelService.DodajHotel("H001", "Grand Hotel", 2020, 5, "1234567890123");
-            hotelService.DodajHotel("H002", "Hotel Kragujevac", 2018, 4, "1234567890123");
-            
-            Console.WriteLine("--- Svi hoteli ---");
-            foreach (var h in hotelService.GetSviHoteli())
+            if (hotelRepo.GetAll().Count == 0)
             {
-                Console.WriteLine($"{h.GetIme()} - {h.GetBrojZvezdica()} zvezdica");
+                hotelService.DodajHotel("H001", "Grand Hotel Kragujevac", 2020, 5, "1234567890123");
+                hotelService.DodajHotel("H002", "Hotel Šumarice", 2018, 4, "1234567890123");
+                hotelService.DodajHotel("H003", "Hotel Park", 2015, 3, "1234567890123");
             }
+            
+            // Pokreni aplikaciju
+            var meni = new KonzolniMeni(authService, korisnikService, hotelService);
+            meni.Pokreni();
         }
     }
 }
